fdee8c7827036bdbd3e1eed4b807559ecc2566ce
Viktor Sava <sleep632@gmail.com>
2021-11-14 21:05:35 +0200

    Add catalog of products

--- src/main/java/com/example/online_store/servlet/CatalogServlet.java
+++ src/main/java/com/example/online_store/servlet/CatalogServlet.java
@@ -1,18 +1,48 @@
 package com.example.online_store.servlet;
 
+import com.example.online_store.dao.CategoryDao;
+import com.example.online_store.model.Category;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-public class CatalogServlet extends AbstractServlet{
+public class CatalogServlet extends AbstractServlet {
+
+    private CategoryDao categoryDao;
+
     public CatalogServlet() {
         super("pages/catalog.jsp");
     }
 
     @Override
+    public void init() {
+        categoryDao = getDaoFactory().getCategoryDao();
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!addCategories(req, resp)) {
+            resp.sendRedirect(getServletContext().getContextPath() + "/category?id=" + req.getParameter("id"));
+            return;
+        }
         forward(req, resp);
+    }
+
+    private boolean addCategories(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Category> categoryList;
+        if (req.getParameter("id") == null) {
+            categoryList = categoryDao.findCategoriesByParentIdByOrderName(getLocale(req, resp), 0);
+        } else {
+            categoryList = categoryDao.findCategoriesByParentIdByOrderName(getLocale(req, resp), Integer.parseInt(req.getParameter("id")));
+        }
+        if (categoryList.size() == 0) {
+            return false;
+        }
+        req.setAttribute("categories", categoryList);
+        return true;
     }
 
 
--- src/main/webapp/pages/catalog.jsp
+++ src/main/webapp/pages/catalog.jsp
@@ -7,7 +7,12 @@
 <jsp:include page="elements/header.jsp" />
 <div class="content">
     <div class="container">
-
+        <c:forEach var="item" items="${categories}">
+            <div class="container card" style="background-image: url(<c:out value="${item.picture}" />)">
+                <span class="card__title"><c:out value="${item.name}" /></span>
+                <a href="${pageContext.request.contextPath}/catalog?id=<c:out value="${item.id}" />"><button class="card__button" type="button">Buy</button></a>
+            </div>
+        </c:forEach>
     </div>
 </div>
 <jsp:include page="elements/footer.jsp" />
