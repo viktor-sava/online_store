e7c37b0e4d63bec454812cb9f2b52d541c1ef010
Viktor Sava <sleep632@gmail.com>
2021-11-14 21:01:52 +0200

    Add tests

--- src/test/java/com/example/online_store/dao/hsqldb/AccountTest.java
+++ src/test/java/com/example/online_store/dao/hsqldb/AccountTest.java
@@ -2,20 +2,21 @@ package com.example.online_store.dao.hsqldb;
 
 import com.example.online_store.dao.AccountDao;
 import com.example.online_store.dao.DAOFactory;
+import com.example.online_store.dao.mysql.MysqlDAOFactory;
 import com.example.online_store.model.Account;
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.Answer;
+import org.mockito.internal.util.reflection.FieldSetter;
 
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.spy;
 
 public class AccountTest {
 
@@ -23,16 +24,17 @@ public class AccountTest {
     private static DataSource dataSource;
 
     @BeforeAll
-    public static void beforeAll() throws SQLException {
-        dataSource = mock(DataSource.class);
-        when(dataSource.getConnection()).then((Answer<Connection>) invocationOnMock ->
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useOldAliasMetadataBehavior=true", "admin", "1234"));
-        try {
-            DAOFactory daoFactory = new MysqlTestDAOFactory(dataSource);
-            accountDao = daoFactory.getAccountDao();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static void beforeAll() throws SQLException, NoSuchFieldException {
+        MysqlConnectionPoolDataSource poolDataSource = new MysqlConnectionPoolDataSource();
+        poolDataSource.setURL("jdbc:mysql://localhost:3306/test");
+        poolDataSource.setUser("admin");
+        poolDataSource.setPassword("1234");
+        DAOFactory daoFactory = spy(new MysqlDAOFactory());
+        FieldSetter fieldSetter = new FieldSetter(daoFactory, MysqlDAOFactory.class.getDeclaredField("dataSource"));
+        fieldSetter.set(poolDataSource);
+
+        dataSource = poolDataSource;
+        accountDao = daoFactory.getAccountDao();
         dropRecords();
     }
 
@@ -54,7 +56,6 @@ public class AccountTest {
                 .email("231@gmail.com")
                 .phone("123412")
                 .build();
-        accountDao.insertAccount(account);
         accountDao.insertAccount(account);
         Assertions.assertNotNull(accountDao.findAccountByEmailOrPhone(account.getEmail(), null));
         Assertions.assertNotNull(accountDao.findAccountByEmailOrPhone(account.getEmail(), account.getPhone()));
